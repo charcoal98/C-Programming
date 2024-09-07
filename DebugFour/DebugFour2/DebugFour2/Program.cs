2ab7443e0958bfc47fc8674feedf222488728eda
charcoal99 <nathanbear99@gmail.com>
2024-09-07 09:13:05 -0500

    Add files via upload

---
+++ DebugFour/DebugFour2/DebugFour2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Program asks user to enter three integers
+// Program displays a message indicating whether the numbers
+// are in sequential order,
+// in reverse order
+// or in neither order
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DebugFour2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            string snum1, snum2, snum3;
+            Write("Enter first number ");
+            snum1 = ReadLine();
+            num1 = Convert.ToInt32(snum1);
+            Write("Enter second number ");
+            snum2 = ReadLine();
+            num2 = Convert.ToInt32(snum2);
+            Write("Enter third number ");
+            snum3 = ReadLine();
+            num3 = Convert.ToInt32(snum3);
+            if (num1 < num2 && num2 < num3)
+                WriteLine("Numbers are in sequential order");
+            else
+               if (num1 > num2 && num2 > num3)
+                WriteLine("Numbers are in reverse order");
+            else
+                WriteLine("Numbers are in neither sequential nor reverse order");
+        }
+    }
+}
---
+++ DebugFour/DebugFour4/DebugFour4/Program.cs
@@ -0,0 +1,51 @@
+﻿// Program computes sales commission based on the following:
+// Sales up to and including $1,000 -- 5% commission
+// Up to and including $5,000 -- 5% on first $1,000 and
+//     7% commission on amount over $1,000
+// up to and including $10,000 -- same as before plus $1,000 bonus
+// over $10,000 - same as all of the above plus
+//    additional $1,500 bonus
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DebugFour4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sales, commission;
+            string inputString;
+            const int LOWSALES = 1000;
+            const int MEDSALES = 5000;
+            const int HIGHSALES = 10000;
+            const double LOWPCT = 0.05;
+            const double MEDPCT = 0.07;
+            const int BONUS1 = 1000;
+            const int BONUS2 = 1500;
+            WriteLine("What was the sales amount? ");
+            sales = Convert.ToDouble(ReadLine());
+            commission = LOWPCT * sales;
+            if (sales <= LOWSALES)
+                commission += (sales - LOWSALES) * MEDPCT;
+            else
+              if (sales <= MEDSALES)
+                commission += 50 + ((sales - LOWSALES) * MEDPCT);
+                else
+                    if (sales <= HIGHSALES) { 
+                        commission += 50 + ((sales - LOWSALES) * MEDPCT);
+                        commission += BONUS1; }
+                    else
+                        if (sales > HIGHSALES){
+                            commission += 50 + ((sales - LOWSALES) * MEDPCT);
+                            commission += BONUS1;
+                            commission += BONUS2;}
+            WriteLine("Sales: {0}\nCommission: {1}",
+              sales.ToString("C"), commission.ToString("C"));
+        }
+    }
+}
