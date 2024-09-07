af983fee89d8718dbba33f3672351457d22d2d2b
charcoal99 <nathanbear99@gmail.com>
2024-09-07 09:17:02 -0500

    Add files via upload

---
+++ 130 2and3/MoveEstimatorGUI/MoveEstimatorGUI/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+using static System.Convert;
+
+namespace MoveEstimatorGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Var
+            const int MRATE = 200, HRATE = 150, MIRATE = 2;
+            double hour, mile, total;
+            //Read
+            mile = Convert.ToDouble(textBox1.Text);
+            hour = Convert.ToDouble(textBox2.Text);
+            //calculation
+            total = MRATE + (hour * HRATE) + (mile * MIRATE);
+            //Display
+            label3.Visible = true;
+            label3.Text = String.Format("For a move taking {0} hours and going {1} miles,\nthe total moving fee is {2}.", hour, mile, total.ToString("c"));
+        }
+    }
+}
---
+++ 130 2and3/ProjectedRaisesGUI/ProjectedRaisesGUI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Convert;
+
+namespace ProjectedRaisesGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Var and Calc
+            const double RAISE = .04;
+            double salary = ToDouble(textBox1.Text);
+            double nSalary = salary + (salary * RAISE);
+            //Display
+            label2.Visible = true;
+            label2.Text = String.Format("Salary increases by 4%, from ${0} to ${1}", salary, nSalary);
+
+        }
+    }
+}
